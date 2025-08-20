a5e997a9cd3632db367176d2feddf22e981066d8
eliranwong <hkwebtech@gmail.com>
2025-08-20 18:23:55 +0100

    minor version changed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import os, shutil
 
 package = "agentmake"
-version="1.0.51"
+version="1.0.52"
 
 # update version info
 info_file = os.path.join(package, "version.txt") # package readme
