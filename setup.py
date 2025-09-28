190baa5376bcaa8ad9ab3b4393b7641c7c6e4fe9
eliranwong <hkwebtech@gmail.com>
2025-09-28 21:35:51 +0100

    support biblemate token

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import os, shutil
 
 package = "agentmake"
-version="1.0.98"
+version="1.0.99"
 
 # update version info
 info_file = os.path.join(package, "version.txt") # package readme
